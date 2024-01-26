d560e2d43d834b09463afb3c317d164521840f75
Gavin Popken <gavinpopkenart@gmail.com>
2024-01-26 13:45:27 -0600

    added MUI dropdown

--- src/components/FavoriteView/FavoriteView.jsx
+++ src/components/FavoriteView/FavoriteView.jsx
@@ -8,8 +8,14 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
 import axios from "axios";
 import { useState, useEffect } from "react";
+
 function FavoriteView() {
   const [favoriteGifs, setFavoriteGifs] = useState([]);
   const fetchFavorites = () => {
@@ -40,20 +46,46 @@ function FavoriteView() {
       });
   };
 
+  const card = (
+    <React.Fragment>
+      <CardContent></CardContent>
+      {/* display here */}
+      <CardActions></CardActions>
+    </React.Fragment>
+  );
+
   return (
     <div>
       {favoriteGifs.map((gif) => (
         <div key={gif.id}>
-          <img src={gif.url} alt={gif.title} />
-          <label htmlFor="categories">Sort to Category:</label>
-          <select name="categories" id="categories" onChange={(event) => setCategories(gif.id, event.target.value)}>
-            <option value="">Choose an option</option>
-            <option value={1}>Wild</option>
-            <option value={2}>Uproarious</option>
-            <option value={3}>Poignant</option>
-            <option value={4}>Felicitous</option>
-            <option value={5}>Whimsical</option>
-          </select>
+
+          <Box sx={{ minWidth: 275 }}>
+            <Card variant="outlined">
+              {/* {card} */}
+              <img src={gif.url} alt={gif.title} />
+              <Stack direction="column" spacing={10}>
+                {/* <Button onChange={() => setIsFavorited(true)} variant="outlined" onClick={handlePost}>FAVORITE</Button> */}
+                <FormControl sx={{ m: 1, minWidth: 80 }}>
+                  <InputLabel id={`label-${gif.id}`}>Choose Category</InputLabel>
+                  <Select
+                    autoWidth
+                    label="category"
+                    value={''}
+                    onChange={(event) => setCategories(gif.id, event.target.value)}
+                  >
+                    <MenuItem value={1}>wild</MenuItem>
+                    <MenuItem value={2}>uproarious</MenuItem>
+                    <MenuItem value={3}>poignant</MenuItem>
+                    <MenuItem value={4}>felicitous</MenuItem>
+                    <MenuItem value={5}>whimsical</MenuItem>
+                  </Select>
+                </FormControl>
+
+
+              </Stack>
+            </Card>
+          </Box>
+
         </div>
       ))}
     </div>
--- src/components/GiphyResults/GiphyResults.jsx
+++ src/components/GiphyResults/GiphyResults.jsx
@@ -1,53 +1,16 @@
 import { useSelector } from "react-redux";
 import * as React from "react";
-import Button from "@mui/material/Button";
-import Stack from "@mui/material/Stack";
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
 import GifCard from "../GifCard/GifCard";
 
 const GiphyResults = () => {
   const results = useSelector((store) => store.input);
 
-  const card = (
-    <React.Fragment>
-      <CardContent></CardContent>
-      {/* display here */}
-      <CardActions></CardActions>
-    </React.Fragment>
-  );
-
   if (results.data) {
     return (
       <>
-        {/* <table>
-                    <tbody>
-                        {results.data.map((gif) => {
-                            return <tr key={gif.id}><td>{gif.embed_url}</td></tr>
-                        })}
-                    </tbody>
-                </table> */}
-
         <div>
           {results.data.map((gif) => {
             return (
-            //   <>
-            //     <Box sx={{ minWidth: 275 }}>
-            //       <Card variant="outlined">
-            //         {card}
-            //         <Stack direction="row" spacing={2}>
-            //           <Button variant="outlined">FAVORITE</Button>
-            //         </Stack>
-            //         <img
-            //           key={gif.id}
-            //           src={gif.images.original.webp}
-            //           alt={gif.title}
-            //         />
-            //       </Card>
-            //     </Box>
-            //   </>
             <GifCard gif={gif} key={gif.id}/>
             );
           })}
